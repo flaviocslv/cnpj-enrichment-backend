690a007c248929a982f461c1facffc5b5c9736b5
flaviocslv <52041688+flaviocslv@users.noreply.github.com>
2025-07-02 15:18:24 -0300

    Update utils.py

--- app/utils.py
+++ app/utils.py
@@ -1,2 +1,5 @@
 def sanitize_cnpj(cnpj: str) -> str:
-    return ''.join(filter(str.isdigit, str(cnpj)))
+    """Remove caracteres não numéricos e garante 14 dígitos com zeros à esquerda."""
+    import re
+    digits = re.sub(r'\D', '', str(cnpj))
+    return digits.zfill(14)[:14]
